2bfd0d0f02d1f35328c444cacc01fffb40ce66be
Full Stack Development <fullstackdev@labs.sce.carleton.ca>
2024-05-03 14:59:03 -0400

    made a timer :D

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,36 @@
 console.clear()
+let time = 60;
+timer()
 let attempts = 0;
-if (localStorage.attempts) {
-    attempts = localStorage.attempts;
+let high = 0;
+if (localStorage.high) {
+    high = localStorage.high;
 } else {
-    localStorage.attempts = attempts;
+    localStorage.high = high;
 }
+document.getElementById("high").innerHTML = "High Score:" + high;
 document.getElementById("attempt").innerHTML = "Attempts:" + attempts;
 document.getElementById("btn").style.marginLeft = (100*Math.random()) + "%";
 document.getElementById("btn").style.marginTop = (48*Math.random()) + "%";
 function buhBye() {
     attempts++
-    localStorage.attempts = attempts;
+    
     document.getElementById("attempt").innerHTML = "Attempts:" + attempts;
     let leftt = (100*Math.random()) + "%";
     document.getElementById("btn").style.marginLeft = leftt;
     let top = (48*Math.random()) + "%";
     document.getElementById("btn").style.marginTop = top;
+}
+function timer(){
+    
+    time--
+    document.getElementById("Timer").innerHTML = "Time Left:" + time + " seconds";
+    if (time<0){
+        alert("You got " + attempts + " attempts! Press OK to retry")
+        high = attempts;
+        localStorage.high = high;
+        document.getElementById("high").innerHTML = "High Score:" + high;
+        location.reload()
+    }
+    setTimeout(timer, 1000)
 }
